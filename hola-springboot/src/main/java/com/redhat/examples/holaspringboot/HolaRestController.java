9755c33a7cbd44df6467050435838370313eaf41
NR43657 <NR43657@INPUHJLP07592.mshome.net>
2018-06-07 16:15:52 +0530

    first commit

---
+++ hola-springboot/src/main/java/com/redhat/examples/holaspringboot/HolaRestController.java
@@ -0,0 +1,23 @@
+package com.redhat.examples.holaspringboot;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HolaRestController {
+	@RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
+	public String hola() throws UnknownHostException {
+		String hostname = null;
+		try {
+			hostname = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			hostname = "unknown";
+		}
+		return "Hola Spring Boot de " + hostname;
+	}
+}
